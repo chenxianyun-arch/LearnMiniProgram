f1aa142952f2baaaa5a1c16a51d2b1014e8a57f5
chenxianyun-arch <1371769065@qq.com>
2020-05-09 14:28:51 +0800

    05_数据绑定初体验

--- pages/about/about.js
+++ pages/about/about.js
@@ -1,7 +1,23 @@
 // miniprogram/pages/about/about.js
 Page({
-  // name: '陈贤云'
   data: {
-    name: "陈贤云"
+    name: "陈贤云",
+    movies:[
+      {id: 1,name: "火隐忍者", price: 23},
+      {id: 2,name: "西瓜太郎", price: 34},
+      {id: 4,name: "喜洋洋",   price: 40},
+      {id: 5,name: "火锅大王", price: 50}
+    ],
+    counter :1
+  },
+  increment(){
+    this.setData({
+      counter:this.data.counter+1
+    })
+  },
+  decrement(){
+    this.setData({
+      counter:this.data.counter-1
+    })
   }
 })
