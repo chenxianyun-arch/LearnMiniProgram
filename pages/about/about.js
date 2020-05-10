c9e14d004aa939fe5ab4a7a5aab2061a3c38baa4
chenxianyun-arch <1371769065@qq.com>
2020-05-10 21:57:03 +0800

    小程序的注册和配置

--- pages/about/about.js
+++ pages/about/about.js
@@ -1,66 +1,23 @@
 // miniprogram/pages/about/about.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    name: "陈贤云",
+    movies:[
+      {id: 1,name: "火隐忍者", price: 23},
+      {id: 2,name: "西瓜太郎", price: 34},
+      {id: 4,name: "喜洋洋",   price: 40},
+      {id: 5,name: "火锅大王", price: 50}
+    ],
+    counter :1
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  increment(){
+    this.setData({
+      counter:this.data.counter+1
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  decrement(){
+    this.setData({
+      counter:this.data.counter-1
+    })
   }
 })
---
+++ pages/category/category.js
@@ -0,0 +1,69 @@
+// pages/category/category.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    console.log("onReady")
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    console.log("onShow")
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    console.log("onHide")
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载，可以在这里发送网络请求
+   */
+  onUnload: function () {
+    console.log("onUnload")
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    console.log("下拉刷新成功")
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    console.log("已经到达底部了")
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  onPageScroll(obj){
+    console.log(obj)
+  }
+})
